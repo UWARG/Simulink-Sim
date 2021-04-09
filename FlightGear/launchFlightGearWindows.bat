e2d73a9eb5b0c0bda5334ef54ce91d3ee20acfac
Nixon Chan <nixonchan22@gmail.com>
2021-04-09 13:05:37 -0400

    FG windows launch script to 2020, fixing cp

--- FlightGear/launchFlightGearWindows.bat
+++ FlightGear/launchFlightGearWindows.bat
@@ -1,5 +1,5 @@
 C:
-cd C:\Program Files\FlightGear 2018.3.6
+cd C:\Program Files\FlightGear 2020.3.6
 
-SET FG_ROOT=C:\Program Files\FlightGear 2018.3.6\data
+SET FG_ROOT=C:\Program Files\FlightGear 2020.3.6\data
 .\\bin\fgfs --generic=socket,in,50,,5500,udp,WargFGPacketStructure --fdm=null --timeofday=noon --disable-real-weather-fetch --wind=0@0 --altitude=80 --lat=21.322 --lon=-157.924
